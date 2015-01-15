ed9084ba83f2357c2bde62b0fc4e01c369e5e730
Elideb <elorahranma@gmail.com>
2015-01-15 21:24:39 +0100

    Improved TimeWrapper test script and scene.

---
+++ Assets/PJEI/TimeWrapper/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 rotation = new Vector3(15f, 15f, 15f);
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.Rotate(this.rotation * Time.deltaTime);
+    }
+}
--- Assets/PJEI/TimeWrapper/Scripts/TimeWrapperTest.cs
+++ Assets/PJEI/TimeWrapper/Scripts/TimeWrapperTest.cs
@@ -4,69 +4,36 @@ using PJEI;
 
 public class TimeWrapperTest : MonoBehaviour {
 
-    void Start() {
-        StartCoroutine(this.RunTimeWarps());
-    }
+    [SerializeField]
+    private Rect instructionsRect = new Rect(50, 100, 500, 100);
 
     [SerializeField]
     private Rect textRect = new Rect(50, 50, 300, 20);
 
     void OnGUI() {
+        GUI.Label(this.instructionsRect, "Press P to pause the game for one second.\nPress S to slow down by half for one game second.\nPress F to fast forward the game by double for one game second");
+
         if (TimeWrapper.Paused)
             GUI.Label(this.textRect, "PAUSED");
         else
             GUI.Label(this.textRect, "Time scale: " + Time.timeScale + " Fixed delta time: " + Time.fixedDeltaTime);
     }
 
-    private System.Collections.IEnumerator RunTimeWarps() {
-        while (true) {
-            int? factor = null;
-            while (factor == null) {
-                factor = GetNumberPressed();
-                yield return null;
-            }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.P))
+            TimeWrapper.Pause(1f);
 
-            int? length = null;
-            while (length == null) {
-                length = GetNumberPressed();
-                yield return null;
-            }
+        if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                TimeWrapper.OverrideTimeForRealSeconds(.5f, 1f);
+            else
+                TimeWrapper.WarpTimeForGameSeconds(.5f, 1f);
 
-            float actualFactor = (float)factor.Value * .1f;
-            float actualLength = (float)length.Value / 2f;
-
-            if (factor.Value == 0) {
-                Debug.Log("Pausing for " + actualLength);
-                TimeWrapper.Pause(actualLength);
-            } else {
-                Debug.Log("Warp to " + actualFactor + " for " + actualLength);
-                TimeWrapper.WarpTimeForGameSeconds(actualFactor, actualLength);
-            }
-        }
+        if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                TimeWrapper.OverrideTimeForRealSeconds(2f, 1f);
+            else
+                TimeWrapper.WarpTimeForGameSeconds(2f, 1f);
     }
 
-    private int? GetNumberPressed() {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-            return 1;
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-            return 2;
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            return 3;
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-            return 4;
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-            return 5;
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-            return 6;
-        if (Input.GetKeyDown(KeyCode.Alpha7))
-            return 7;
-        if (Input.GetKeyDown(KeyCode.Alpha8))
-            return 8;
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-            return 9;
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-            return 0;
-
-        return null;
-    }
 }
