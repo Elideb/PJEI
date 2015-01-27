ad6965e9dcf8269bad811fe63fbc5bd3750da4d7
Elideb <elorahranma@gmail.com>
2015-01-28 00:46:40 +0100

    Created level, which takes care of instantiating aliens, initializing and setting them free.

---
+++ Assets/PJEI/Invaders/Scripts/Alien.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PJEI.Invaders {
+
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Alien : MonoBehaviour {
+
+        public Sprite[] sprites;
+        private int currentSprite = 0;
+
+        private int pixelsPerMove = 0;
+
+        private SpriteRenderer spriteRenderer;
+        private Vector2D currentPos;
+
+        #region State information
+
+        private int movesRemaining = 0;
+        public void SetMovesRemaining(int moves) { movesRemaining = moves; }
+        private bool HasFinishedMoving() { return movesRemaining == 0; }
+
+        private bool movingLeft = true;
+        public void StartMovingLeft() { movingLeft = true; }
+        public void StartMovingRight() { movingLeft = false; }
+        public bool IsMovingLeft() { return movingLeft; }
+
+        #endregion
+
+        public void Initialize(Vector2D initialPosition, int firstSprite, int pixelsPerMove) {
+            this.pixelsPerMove = pixelsPerMove;
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+            SetPosition(initialPosition);
+            SetSprite(firstSprite);
+        }
+
+        public void Run() {
+
+        }
+
+        public void Move(Vector2D distance) {
+            SetPosition(new Vector2D(currentPos.x + distance.x, currentPos.y + distance.y));
+            SetSprite(currentSprite + 1);
+        }
+
+        private void SetPosition(Vector2D position) {
+            currentPos = position;
+            this.transform.position = new Vector3(pixelsPerMove * position.x, (pixelsPerMove / 2) * position.y, 0);
+        }
+
+        private void SetSprite(int spriteCount) {
+            currentSprite = spriteCount % sprites.Length;
+            spriteRenderer.sprite = sprites[currentSprite];
+        }
+    }
+}
---
+++ Assets/PJEI/Invaders/Scripts/Level.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace PJEI.Invaders {
+
+    public class Level : MonoBehaviour {
+        public Alien[] lines;
+        public int width = 11;
+        public int pixelsBetweenAliens = 48;
+
+        private Alien[] aliens;
+
+        void Start() {
+            StartCoroutine(InitGame());
+        }
+
+        private System.Collections.IEnumerator InitGame() {
+            aliens = new Alien[lines.Length * width];
+            for (int i = lines.Length - 1; i >= 0; --i) {
+                for (int j = 0; j < width; ++j) {
+                    var alien = (Alien)Instantiate(lines[i]);
+                    alien.Initialize(new Vector2D(j - width / 2, -i + lines.Length / 2), i, pixelsBetweenAliens);
+
+                    aliens[i * width + j] = alien;
+
+                    yield return new WaitForSeconds(.1f);
+                }
+            }
+
+            foreach (var alien in aliens)
+                alien.Run();
+        }
+    }
+
+}
