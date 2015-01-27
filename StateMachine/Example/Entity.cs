4c7df573ecb866fb721d70da8286ccb95df7c355
Elideb <elorahranma@gmail.com>
2015-01-27 16:58:45 +0100

    Squashed 'Assets/elideb/StateMachine/' content from commit cca23bd
    git-subtree-dir: Assets/elideb/StateMachine
    git-subtree-split: cca23bd3dd5d4772c93e3efdf6f5c5c8fe9db9fc

---
+++ StateMachine/Example/Entity.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace StateMachine.Example {
+
+    class Entity : IStateful<Entity> {
+
+        private StateMachine<Entity> stateMachine = null;
+
+        private float distanceToTarget = 0;
+        public float DistanceToTarget {
+            get { return distanceToTarget; }
+            private set { distanceToTarget = value; }
+        }
+
+        private Entity target = null;
+        public Entity Target {
+            get {
+                return target;
+            }
+            private set {
+                target = value;
+                if (target != null) {
+                    DistanceToTarget = DistanceTo( target );
+                }
+            }
+        }
+
+        private int phrasesLeft;
+
+        public int PhrasesLeft {
+            get { return phrasesLeft; }
+            set { phrasesLeft = value; }
+        }
+
+        public void Initialize() {
+
+            stateMachine = new StateMachine<Entity>( this, EntityStates.Idle )
+                .AddTransitions(
+                    Transition<Entity>
+                        .FromAny()
+                        .Except( EntityStates.Idle, EntityStates.Talk )
+                        .To( EntityStates.Idle )
+                        .When( HasNoTarget ),
+                    Transition<Entity>
+                        .From( EntityStates.Idle )
+                        .To( EntityStates.Walk )
+                        .When( HasTarget ),
+                    Transition<Entity>
+                        .From( EntityStates.Walk )
+                        .To( EntityStates.Talk )
+                        .When( IsTargetInRange ),
+                    Transition<Entity>
+                        .From( EntityStates.Talk )
+                        .To( EntityStates.Idle )
+                        .When( DoneTalking ) );
+
+        }
+
+        public void Update() {
+            stateMachine.Update();
+        }
+
+        public float DistanceTo(Entity other) {
+            return 15f; // Properly calculating distance requires positions...
+        }
+
+        public void Move(float distance) {
+            distanceToTarget -= distance;
+        }
+
+        #region State condition methods
+
+        public bool IsTargetOutOfRange() {
+            return DistanceToTarget > 0.01f;
+        }
+
+        public bool IsTargetInRange() {
+            return !IsTargetOutOfRange();
+        }
+
+        public bool HasNoTarget() {
+            return Target == null;
+        }
+
+        public bool HasTarget() {
+            return !HasNoTarget();
+        }
+
+        public bool DoneTalking() {
+            return PhrasesLeft == 0;
+        }
+
+        #endregion
+
+        #region IStateful<Entity> Members
+
+        public StateMachine<Entity> GetStateMachine() {
+            return stateMachine;
+        }
+
+        #endregion
+    }
+
+}
