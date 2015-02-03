b41dad75fb5780a06124727d6c8abbb1e9d5c71e
Elideb <elorahranma@gmail.com>
2015-02-03 02:33:38 +0100

    Small navmesh demo.

---
+++ Assets/PJEI/NavMeshes/Scripts/Agent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Agent : MonoBehaviour {
+
+    private NavMeshAgent agent = null;
+
+    private int layerToPass;
+
+    private GameObject origin;
+    private GameObject destiny;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        layerToPass = LayerMask.NameToLayer("Igorne Raycase");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        NavMeshHit rayHit;
+        bool touched = NavMesh.Raycast(Camera.main.transform.position, Camera.main.transform.forward * 1000, out rayHit, layerToPass);
+        if (touched) {
+            if (agent.SetDestination(rayHit.position)) {
+                Debug.Log("Moving from " + transform.position + " to " + rayHit.position);
+                if (origin == null)
+                    origin = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                origin.transform.position = transform.position;
+
+                if (destiny == null)
+                    destiny = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                destiny.transform.position = agent.destination;
+            } else {
+                Debug.LogWarning("No path found");
+            }
+        } else {
+            Debug.LogWarning("Touched out of navmesh");
+        }
+    }
+}
---
+++ Assets/PJEI/NavMeshes/Scripts/SimpleMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += new Vector3(0, 0, -5 * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += new Vector3(0, 0, 5 * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += new Vector3(-5 * Time.deltaTime, 0, 0);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(5 * Time.deltaTime, 0, 0);
+        else if (Input.GetKey(KeyCode.LeftControl))
+            transform.position += new Vector3(0, -5 * Time.deltaTime, 0);
+        else if (Input.GetKey(KeyCode.LeftShift))
+            transform.position += new Vector3(0, 5 * Time.deltaTime, 0);
+    }
+}
